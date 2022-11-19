ad04bcc0210bed27c544b8bc7725f381c88e5192
Domenick <99255313+biangjo30jod@users.noreply.github.com>
2022-11-19 14:14:41 +0800

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-51f0a12b472b45a6ad1af0fe77204d55}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
